e3ee71e6a4d31a998d233edfb93182e43df73af6
Suganya <suganyas62@gmail.com>
2026-06-09 08:02:46 +0530

    updated in playwright.config.ts

--- playwright.config.ts
+++ playwright.config.ts
@@ -13,7 +13,7 @@ import { defineConfig, devices } from '@playwright/test';
  */
 export default defineConfig({
    snapshotPathTemplate:'{testDir}/../visual/{arg}{ext}',
-   //Adding new lines
+   //Adding new lines in Suganya Branch
   //retries:3,
   expect: {timeout:2000},
   //timeout:40000,
